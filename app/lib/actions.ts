aecd6020a8627564fe87bec081845c8291b1e5b1
Rei Sugiura <reibomaru@gmail.com>
2024-01-07 19:59:20 +0900

    feat: added auth with next-auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
@@ -105,4 +107,23 @@ export async function deleteInvoice(id: string) {
     };
   }
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }

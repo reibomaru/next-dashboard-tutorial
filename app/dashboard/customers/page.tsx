b3f034652d5ac40ad87e28ff782f7fb0aca5168b
Rei Sugiura <reibomaru@gmail.com>
2024-01-09 22:42:00 +0900

    feat: impl button

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
+'use client';
+
+import { Button } from '@chakra-ui/react';
+
 export default function Page() {
-  return <p>Customers Page</p>;
+  return (
+    <>
+      <h1>Customer page</h1>
+      <Button onClick={() => alert('called')}>ボタン</Button>
+    </>
+  );
 }
